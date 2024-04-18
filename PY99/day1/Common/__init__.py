59b760b4e1feaa27b2362b00c0d45466560ac8e0
qqaabb123 <2500300974@qq.com>
2024-04-18 17:16:21 +0800

    删除PY99

--- PY99/day1/Common/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2024/4/11 20:48
-@Auth ： 荣
-@File ：__init__.py.py
-@IDE ：PyCharm
-@Motto：ABC(Always Be Coding)
-"""
--- PY99/day1/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2024/4/11 18:52
-@Auth ： 荣
-@File ：__init__.py.py
-@IDE ：PyCharm
-@Motto：ABC(Always Be Coding)
-"""
